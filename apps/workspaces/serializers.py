3656afb86109649cf304e85897c8c9adc91c8803
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2023-10-31 09:04:04 +0530

    Docker Setup (#3)
    * Docker Setup
    
    * User model initilised with fyle_rest_auth (#4)
    
    * User model initilised with fyle_rest_auth
    
    * Pytest Setup and flake8 added (#5)
    
    * Pytest Setup and flake8 added
    
    * Workspace APIs Added (#6)
    
    * Workspace APIs Added
    
    * Github action added for tests
    
    * Export Settings APIs

---
+++ apps/workspaces/serializers.py
@@ -0,0 +1,98 @@
+"""
+Workspace Serializers
+"""
+from django.core.cache import cache
+from rest_framework import serializers
+from fyle_rest_auth.helpers import get_fyle_admin
+from fyle_rest_auth.models import AuthToken
+
+from ms_business_central_api.utils import assert_valid
+from apps.workspaces.models import (
+    Workspace,
+    FyleCredential,
+    ExportSetting
+)
+from apps.users.models import User
+from apps.fyle.helpers import get_cluster_domain
+
+
+class WorkspaceSerializer(serializers.ModelSerializer):
+    """
+    Workspace serializer
+    """
+    class Meta:
+        model = Workspace
+        fields = '__all__'
+        read_only_fields = ('id', 'name', 'org_id', 'created_at', 'updated_at', 'user')
+
+    def create(self, validated_data):
+        """
+        Update workspace
+        """
+        access_token = self.context['request'].META.get('HTTP_AUTHORIZATION')
+        user = self.context['request'].user
+
+        # Getting user profile using the access token
+        fyle_user = get_fyle_admin(access_token.split(' ')[1], None)
+
+        # getting name, org_id, currency of Fyle User
+        name = fyle_user['data']['org']['name']
+        org_id = fyle_user['data']['org']['id']
+
+        # Checking if workspace already exists
+        workspace = Workspace.objects.filter(org_id=org_id).first()
+
+        if workspace:
+            # Adding user relation to workspace
+            workspace.user.add(User.objects.get(user_id=user))
+            cache.delete(str(workspace.id))
+        else:
+            workspace = Workspace.objects.create(
+                name=name,
+                org_id=org_id,
+            )
+
+            workspace.user.add(User.objects.get(user_id=user))
+
+            auth_tokens = AuthToken.objects.get(user__user_id=user)
+
+            cluster_domain = get_cluster_domain(auth_tokens.refresh_token)
+
+            FyleCredential.objects.update_or_create(
+                refresh_token=auth_tokens.refresh_token,
+                workspace_id=workspace.id,
+                cluster_domain=cluster_domain
+            )
+
+        return workspace
+
+
+class ExportSettingsSerializer(serializers.ModelSerializer):
+    """
+    Export Settings serializer
+    """
+    class Meta:
+        model = ExportSetting
+        fields = '__all__'
+        read_only_fields = ('id', 'workspace', 'created_at', 'updated_at')
+
+    def create(self, validated_data):
+        """
+        Create Export Settings
+        """
+        assert_valid(validated_data, 'Body cannot be null')
+        workspace_id = self.context['request'].parser_context.get('kwargs').get('workspace_id')
+
+        export_settings, _ = ExportSetting.objects.update_or_create(
+            workspace_id=workspace_id,
+            defaults=validated_data
+        )
+
+        # Update workspace onboarding state
+        workspace = export_settings.workspace
+
+        if workspace.onboarding_state == 'EXPORT_SETTINGS':
+            workspace.onboarding_state = 'IMPORT_SETTINGS'
+            workspace.save()
+
+        return export_settings
--- apps/workspaces/views.py
+++ apps/workspaces/views.py
@@ -1,3 +1,82 @@
-from django.shortcuts import render
+import logging
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.views import Response, status
+
+from fyle_rest_auth.utils import AuthUtils
+
+
+from ms_business_central_api.utils import assert_valid
+from apps.workspaces.models import (
+    Workspace,
+    ExportSetting
+)
+from apps.workspaces.serializers import (
+    WorkspaceSerializer,
+    ExportSettingsSerializer
+)
+
+
+logger = logging.getLogger(__name__)
+logger.level = logging.INFO
+
+User = get_user_model()
+auth_utils = AuthUtils()
+
+
+class WorkspaceView(generics.CreateAPIView, generics.RetrieveAPIView):
+    """
+    Create Retrieve Workspaces
+    """
+    serializer_class = WorkspaceSerializer
+
+    def get_object(self):
+        """
+        return workspace object for the given org_id
+        """
+        user_id = self.request.user
+
+        org_id = self.request.query_params.get('org_id')
+
+        assert_valid(org_id is not None, 'org_id is missing')
+
+        workspace = Workspace.objects.filter(org_id=org_id, user__user_id=user_id).first()
+
+        assert_valid(
+            workspace is not None,
+            'Workspace not found or the user does not have access to workspaces'
+        )
+
+        return workspace
+
+
+class ReadyView(generics.RetrieveAPIView):
+    """
+    Ready call to check if the api is ready
+    """
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        """
+        Ready call
+        """
+        Workspace.objects.first()
+
+        return Response(
+            data={
+                'message': 'Ready'
+            },
+            status=status.HTTP_200_OK
+        )
+
+
+class ExportSettingView(generics.CreateAPIView, generics.RetrieveAPIView):
+    """
+    Retrieve or Create Export Settings
+    """
+    serializer_class = ExportSettingsSerializer
+    lookup_field = 'workspace_id'
+
+    queryset = ExportSetting.objects.all()
