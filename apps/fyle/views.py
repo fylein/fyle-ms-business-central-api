1d1c88e7e59f189531a281a382346b07eda8c49d
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2023-11-14 17:16:30 +0530

    Fyle Import attributes API (#21)

--- apps/fyle/views.py
+++ apps/fyle/views.py
@@ -1,7 +1,7 @@
 import logging
 from rest_framework import generics
 from ms_business_central_api.utils import LookupFieldMixin
-from apps.fyle.serializers import ExpenseFilterSerializer
+from apps.fyle.serializers import ExpenseFilterSerializer, ImportFyleAttributesSerializer
 from apps.fyle.models import ExpenseFilter
 
 logger = logging.getLogger(__name__)
@@ -24,3 +24,10 @@ class ExpenseFilterDeleteView(generics.DestroyAPIView):
 
     queryset = ExpenseFilter.objects.all()
     serializer_class = ExpenseFilterSerializer
+
+
+class ImportFyleAttributesView(generics.CreateAPIView):
+    """
+    Import Fyle Attributes View
+    """
+    serializer_class = ImportFyleAttributesSerializer
