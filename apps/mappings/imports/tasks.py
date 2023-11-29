590098036c912be5e0fbbb6855fd4a944333cd0e
ruuushhh <rushikeshtayade2001@gmail.com>
2023-11-29 21:26:18 +0530

    Import Categories support added

---
+++ apps/mappings/imports/tasks.py
@@ -0,0 +1,26 @@
+from apps.mappings.imports.modules.categories import Category
+from apps.mappings.imports.modules.expense_custom_fields import ExpenseCustomField
+from apps.mappings.models import ImportLog
+
+SOURCE_FIELD_CLASS_MAP = {
+    'CATEGORY': Category,
+}
+
+
+def trigger_import_via_schedule(workspace_id: int, destination_field: str, source_field: str, is_custom: bool = False):
+    """
+    Trigger import via schedule
+    :param workspace_id: Workspace id
+    :param destination_field: Destination field
+    :param source_field: Type of attribute (e.g. 'CATEGORY', 'LOCATION', 'VENDOR', 'EMPLOYEE')
+    """
+    import_log = ImportLog.objects.filter(workspace_id=workspace_id, attribute_type=source_field).first()
+    sync_after = import_log.last_successful_run_at if import_log else None
+
+    if is_custom:
+        item = ExpenseCustomField(workspace_id, source_field, destination_field, sync_after)
+        item.trigger_import()
+    else:
+        module_class = SOURCE_FIELD_CLASS_MAP[source_field]
+        item = module_class(workspace_id, destination_field, sync_after)
+        item.trigger_import()
