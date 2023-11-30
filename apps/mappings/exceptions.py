71deafc8675ab17539294fd1a0ba61ea08e77441
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2023-11-30 15:46:48 +0530

    Import base files added (#39)
    * Import base files added
    
    * Import Categories support added (#40)
    
    * Import Categories support added
    
    * Import merchants support added (#41)
    
    * Import merchants support added
    
    * Import cost centers support added (#42)
    
    * Import cost centers support added
    
    * Import project support added (#43)
    
    * Import project support added
    
    * Schedule, queues signals configured (#44)
    
    * Schedule, queues signals configured
    
    * Imports bugs fixed (#45)
    
    * Imports bugs fixed
    
    * Comments resolved

---
+++ apps/mappings/exceptions.py
@@ -0,0 +1,65 @@
+import logging
+import traceback
+
+from dynamics.exceptions.dynamics_exceptions import InvalidTokenError
+from fyle.platform.exceptions import InternalServerError
+from fyle.platform.exceptions import InvalidTokenError as FyleInvalidTokenError
+from fyle.platform.exceptions import WrongParamsError
+
+from apps.mappings.models import ImportLog
+from apps.workspaces.models import BusinessCentralCredentials
+
+logger = logging.getLogger(__name__)
+logger.level = logging.INFO
+
+
+def handle_import_exceptions(func):
+    def new_fn(expense_attribute_instance, *args):
+        import_log: ImportLog = args[0]
+        workspace_id = import_log.workspace_id
+        attribute_type = import_log.attribute_type
+        error = {
+            'task': 'Import {0} to Fyle and Auto Create Mappings'.format(attribute_type),
+            'workspace_id': workspace_id,
+            'message': None,
+            'response': None
+        }
+        try:
+            return func(expense_attribute_instance, *args)
+        except WrongParamsError as exception:
+            error['message'] = exception.message
+            error['response'] = exception.response
+            error['alert'] = True
+            import_log.status = 'FAILED'
+
+        except (BusinessCentralCredentials.DoesNotExist, InvalidTokenError):
+            error['message'] = 'Invalid Token or Business central credentials does not exist workspace_id - {0}'.format(workspace_id)
+            error['alert'] = False
+            import_log.status = 'FAILED'
+
+        except FyleInvalidTokenError:
+            error['message'] = 'Invalid Token for fyle'
+            error['alert'] = False
+            import_log.status = 'FAILED'
+
+        except InternalServerError:
+            error['message'] = 'Internal server error while importing to Fyle'
+            error['alert'] = True
+            import_log.status = 'FAILED'
+
+        except Exception:
+            response = traceback.format_exc()
+            error['message'] = 'Something went wrong'
+            error['response'] = response
+            error['alert'] = False
+            import_log.status = 'FATAL'
+
+        if error['alert']:
+            logger.error(error)
+        else:
+            logger.info(error)
+
+        import_log.error_log = error
+        import_log.save()
+
+    return new_fn
---
+++ apps/mappings/imports/schedules.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+from django_q.models import Schedule
+from fyle_accounting_mappings.models import MappingSetting
+
+from apps.workspaces.models import ImportSetting
+
+
+def schedule_or_delete_fyle_import_tasks(import_settings: ImportSetting, mapping_setting_instance: MappingSetting = None):
+    """
+    Schedule or delete Fyle import tasks based on the configuration.
+    :param configuration: Workspace Configuration Instance
+    :param instance: Mapping Setting Instance
+    :return: None
+    """
+    task_to_be_scheduled = None
+    # Check if there is a task to be scheduled
+    if mapping_setting_instance and mapping_setting_instance.import_to_fyle:
+        task_to_be_scheduled = mapping_setting_instance
+
+    if task_to_be_scheduled or import_settings.import_categories:
+        Schedule.objects.update_or_create(
+            func='apps.mappings.imports.queues.chain_import_fields_to_fyle',
+            args='{}'.format(import_settings.workspace_id),
+            defaults={
+                'schedule_type': Schedule.MINUTES,
+                'minutes': 24 * 60,
+                'next_run': datetime.now()
+            }
+        )
+        return
+
+    import_fields_count = MappingSetting.objects.filter(
+        import_to_fyle=True,
+        workspace_id=import_settings.workspace_id,
+        source_field__in=['CATEGORY', 'PROJECT', 'COST_CENTER']
+    ).count()
+
+    custom_field_import_fields_count = MappingSetting.objects.filter(
+        import_to_fyle=True,
+        workspace_id=import_settings.workspace_id,
+        is_custom=True
+    ).count()
+
+    # If the import fields count is 0, delete the schedule
+    if import_fields_count == 0 and custom_field_import_fields_count == 0:
+        Schedule.objects.filter(
+            func='apps.mappings.imports.queues.chain_import_fields_to_fyle',
+            args='{}'.format(import_settings.workspace_id)
+        ).delete()
