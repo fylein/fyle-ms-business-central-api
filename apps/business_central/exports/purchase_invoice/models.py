29e29d1987804fa827a58cc7aaaac12e545ee459
ruuushhh <rushikeshtayade2001@gmail.com>
2024-01-12 15:57:15 +0530

    Purchase Invoice Export

--- apps/business_central/exports/purchase_invoice/models.py
+++ apps/business_central/exports/purchase_invoice/models.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from django.db import models
 from fyle_accounting_mappings.models import CategoryMapping
 
 from apps.accounting_exports.models import AccountingExport
 from apps.business_central.exports.base_model import BaseExportModel
 from apps.fyle.models import Expense
-from apps.workspaces.models import AdvancedSetting
+from apps.workspaces.models import AdvancedSetting, ExportSetting
 from ms_business_central_api.models.fields import CustomDateTimeField, FloatNullField, StringNullField, TextNotNullField
 
 
@@ -13,26 +15,23 @@ class PurchaseInvoice(BaseExportModel):
     Purchase Invoice Model
     """
 
-    accounting_export = models.OneToOneField(AccountingExport, on_delete=models.PROTECT, help_text='Accounting Export reference')
+    accounting_export = models.OneToOneField(AccountingExport, on_delete=models.PROTECT, help_text='Accounting Export reference', related_name='purchase_invoice')
     accounting_date = CustomDateTimeField(help_text='accounting date of purchase invoice')
     amount = FloatNullField(help_text='Total Amount of the invoice')
     code = StringNullField(max_length=10, help_text='unique code for invoice')
-    description = TextNotNullField(help_text='description for the invoice')
     invoice_date = CustomDateTimeField(help_text='date of invoice')
-    tax_amount = FloatNullField(help_text='total tax amount of the invoice')
-    vendor_id = StringNullField(help_text='id of vendor')
+    vendor_number = StringNullField(help_text='id of vendor')
 
     class Meta:
         db_table = 'purchase_invoices'
 
     @classmethod
-    def create_or_update_object(self, accounting_export: AccountingExport):
+    def create_or_update_object(self, accounting_export: AccountingExport, advance_setting: AdvancedSetting, export_settings: ExportSetting):
         """
         Create Purchase Invoice
         :param accounting_export: expense group
         :return: purchase invoices object
         """
-        description = accounting_export.description
 
         vendor_id = self.get_vendor_id(accounting_export=accounting_export)
         amount = self.get_total_amount(accounting_export=accounting_export)
@@ -42,8 +41,7 @@ class PurchaseInvoice(BaseExportModel):
             accounting_export=accounting_export,
             defaults={
                 'amount': amount,
-                'vendor_id': vendor_id,
-                'description': description,
+                'vendor_number': vendor_id,
                 'invoice_date': invoice_date,
                 'workspace_id': accounting_export.workspace_id
             }
@@ -62,7 +60,6 @@ class PurchaseInvoiceLineitems(BaseExportModel):
     expense = models.OneToOneField(Expense, on_delete=models.PROTECT, help_text='Reference to Expense')
     amount = FloatNullField(help_text='Amount of the invoice')
     description = TextNotNullField(help_text='description for the invoice')
-    tax_amount = FloatNullField(help_text='tax amount of the invoice')
 
     class Meta:
         db_table = 'purchase_invoice_lineitems'
@@ -75,7 +72,7 @@ class PurchaseInvoiceLineitems(BaseExportModel):
         :return: purchase invoices object
         """
 
-        expenses = accounting_export.expenses.all()
+        expenses: List[Expense] = accounting_export.expenses.all()
         purchase_invoice = PurchaseInvoice.objects.get(accounting_export=accounting_export)
 
         purchase_invoice_lineitem_objects = []
@@ -86,7 +83,7 @@ class PurchaseInvoiceLineitems(BaseExportModel):
                 workspace_id=accounting_export.workspace_id
             ).first()
 
-            description = self.get_expense_purpose(accounting_export.workspace_id, lineitem, lineitem.category, advance_setting)
+            description = self.get_expense_purpose(lineitem, lineitem.category, advance_setting)
 
             purchase_invoice_lineitem_object, _ = PurchaseInvoiceLineitems.objects.update_or_create(
                 purchase_invoice_id=purchase_invoice.id,
---
+++ apps/business_central/exports/purchase_invoice/tasks.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+from typing import Dict, List
+
+from apps.accounting_exports.models import AccountingExport
+from apps.business_central.exceptions import handle_business_central_exceptions
+from apps.business_central.exports.accounting_export import AccountingDataExporter
+from apps.business_central.exports.helpers import load_attachments
+from apps.business_central.exports.purchase_invoice.models import PurchaseInvoice, PurchaseInvoiceLineitems
+from apps.business_central.exports.purchase_invoice.queues import check_accounting_export_and_start_import
+from apps.business_central.utils import BusinessCentralConnector
+from apps.workspaces.models import BusinessCentralCredentials
+
+
+class ExportPurchaseInvoice(AccountingDataExporter):
+    '''
+    Class for handling the export of Purchase Invoice to Business Central.
+    Extends the base AccountingDataExporter class.
+    '''
+
+    def __init__(self):
+        super().__init__()  # Call the constructor of the parent class
+        self.body_model = PurchaseInvoice
+        self.lineitem_model = PurchaseInvoiceLineitems
+
+    def trigger_export(self, workspace_id, accounting_export_ids):
+        '''
+        Trigger the import process for the Project module.
+        '''
+        check_accounting_export_and_start_import(workspace_id, accounting_export_ids)
+
+    def __construct_purchase_invoice(self, body: PurchaseInvoice, lineitems: List[PurchaseInvoiceLineitems]) -> Dict:
+        '''
+        Construct the payload for the direct invoice.
+        :param expense_report: ExpenseReport object extracted from database
+        :param expense_report_lineitems: ExpenseReportLineitem objects extracted from database
+        :return: constructed expense_report
+        '''
+        batch_purchase_invoice_lineitem_payload = []
+
+        purchase_invoice_payload = {
+            'vendorNumber': body.vendor_number,
+            'invoiceDate': body.invoice_date,
+            'postingDate': datetime.now().strftime("%Y-%m-%d")
+        }
+
+        for lineitem in lineitems:
+            purchase_invoice_lineitem_payload = {
+                "lineType": "Account",
+                'lineObjectNumber': lineitem.accounts_payable_account_id,
+                'unitCost': lineitem.amount,
+                'quantity': 1,
+                'description': lineitem.description if lineitem.description else ''
+            }
+
+            batch_purchase_invoice_lineitem_payload.append(purchase_invoice_lineitem_payload)
+
+        return purchase_invoice_payload, batch_purchase_invoice_lineitem_payload
+
+    def post(self, accounting_export, item, lineitem):
+        '''
+        Export the Journal Entry to Business Central.
+        '''
+
+        purchase_invoice_payload, batch_purchase_invoice_payload = self.__construct_purchase_invoice(item, lineitem)
+        business_central_credentials = BusinessCentralCredentials.objects.filter(workspace_id=accounting_export.workspace_id).first()
+        # Establish a connection to Business Central
+        business_central_connection = BusinessCentralConnector(business_central_credentials, accounting_export.workspace_id)
+
+        response = business_central_connection.post_purchase_invoice(purchase_invoice_payload, batch_purchase_invoice_payload)
+
+        expenses = accounting_export.expenses.all()
+
+        # Load attachments to Business Central
+        for expense in expenses:
+            load_attachments(
+                business_central_connection,
+                response["purchase_invoice_response"]["id"],
+                "Purchase Invoice",
+                expense,
+                accounting_export)
+
+        return response
+
+
+@handle_business_central_exceptions()
+def create_purchase_invoice(accounting_export: AccountingExport):
+    '''
+    Helper function to create and export a journal entry.
+    '''
+    export_purchase_invoice_instance = ExportPurchaseInvoice()
+
+    # Create and export the journal entry using the base class method
+    exported_purchase_invoice = export_purchase_invoice_instance.create_business_central_object(accounting_export=accounting_export)
+
+    return exported_purchase_invoice
