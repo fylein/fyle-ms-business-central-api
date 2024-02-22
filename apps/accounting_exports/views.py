f4eab4772f83365f5347ec62577d822210884371
ruuushhh <rushikeshtayade2001@gmail.com>
2024-02-23 00:37:00 +0530

    Error API fix

--- apps/accounting_exports/views.py
+++ apps/accounting_exports/views.py
@@ -51,8 +51,26 @@ class AccountingExportSummaryView(generics.RetrieveAPIView):
     serializer_class = AccountingExportSummarySerializer
 
 
-class ErrorsView(LookupFieldMixin, generics.ListAPIView):
+class ErrorsView(generics.ListAPIView):
     serializer_class = ErrorSerializer
-    queryset = Error.objects.all()
-    filter_backends = (DjangoFilterBackend,)
-    filterset_fields = {"type": {"exact"}, "is_resolved": {"exact"}}
+    pagination_class = None
+
+    def get_queryset(self):
+        type = self.request.query_params.get('type')
+
+        is_resolved = self.request.query_params.get('is_resolved', None)
+
+        params = {'workspace__id': self.kwargs.get('workspace_id')}
+
+        if is_resolved and is_resolved.lower() == 'true':
+            is_resolved = True
+        elif is_resolved and is_resolved.lower() == 'false':
+            is_resolved = False
+
+        if is_resolved is not None:
+            params['is_resolved'] = is_resolved
+
+        if type:
+            params['type'] = type
+
+        return Error.objects.filter(**params)
