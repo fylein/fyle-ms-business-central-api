8a132a7139ca1486ca56f9a1e41739f536c813c4
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-07-12 13:15:17 +0530

    fix null refresh token

--- apps/workspaces/tasks.py
+++ apps/workspaces/tasks.py
@@ -19,7 +19,7 @@ logger.level = logging.INFO
 
 def async_update_fyle_credentials(fyle_org_id: str, refresh_token: str):
     fyle_credentials = FyleCredential.objects.filter(workspace__org_id=fyle_org_id).first()
-    if fyle_credentials:
+    if fyle_credentials and refresh_token:
         fyle_credentials.refresh_token = refresh_token
         fyle_credentials.save()
 
